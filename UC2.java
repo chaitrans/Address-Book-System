eadd5bfc8e204e777aef162e997c898284d30570
chaitra845 <chaitrachytra@gmail.com>
2021-06-22 00:25:04 +0530

    Added New Contact to List

---
+++ UC2.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Getting input from user and set that input in the constructor
+ * And adding contact to address book
+ * @author Chaitra NS
+ * @since 21 Jun 2021
+ * @version 2.0
+ */
+
+public class AddressSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Contact> contactList = new ArrayList<Contact>();
+
+        Contact contact = new Contact();
+
+       System.out.println("Enter the First Name : " );
+        String firstName = sc.next();
+        contact.setFirstName(firstName);
+
+        System.out.println("Enter the Last Name : " );
+        String lastName = sc.next();
+       contact.setLastName(lastName);
+
+        System.out.println("Enter the Address : " );
+        String address = sc.next();
+        contact.setAddress(address);
+
+        System.out.println("Enter the City : " );
+        String city = sc.next();
+        contact.setCity(city);
+
+        System.out.println("Enter the State : " );
+        String state = sc.next();
+        contact.setState(state);
+
+        System.out.println("Enter the ZIP Code : " );
+        long zipCode = sc.nextLong();
+        contact.setZip(zipCode);
+
+       System.out.println("Enter the Phone Number : " );
+      long phoneNumber = sc.nextLong();
+        contact.setPhoneNumber(phoneNumber);
+
+        System.out.println("Enter the E-mail: " );
+        String email = sc.next();
+        contact.setEmail(email);
+
+        contactList.add(contact);
+
+        for (Contact c : contactList){
+            System.out.println(c.getFirstName() + " " + c.getLastName() + " " + c.getAddress() + " " + c.getCity() + " " + c.getState() + " " + c.getPhoneNumber() + " " + c.getZip() + " " + c.getEmail());
+        }
+    }
+}
