2674d243b90ef30ad866fc85a4916a9f04fed451
chaitra845 <chaitrachytra@gmail.com>
2021-06-22 00:30:26 +0530

    Added Delete Option

--- UC4.java
+++ UC4.java
@@ -5,30 +5,32 @@ import java.util.Scanner;
  * Getting input from user and set that input in the constructor
  * And adding contact to address book
  * And editing person details
+ * And deleting the contact
  * @author Chaitra NS
  * @since 21 Jun 2021
  * @version 2.0
  */
+
 
 public class AddressSystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Contact> contactList = new ArrayList<Contact>();
 
-       Contact contact = new Contact();
+        Contact contact = new Contact();
 
-       System.out.println("Enter 1 to add contact \nEnter 2 to edit");
+        System.out.println("Enter 1 to add contact \nEnter 2 to edit\nEnter 3 to delete");
         System.out.println("Enter the option : ");
         int option = sc.nextInt();
         switch (option){
             case 1 :
                 System.out.println("Add");
                 System.out.println("Enter the First Name : " );
-                String firstName = sc.next();
+               String firstName = sc.next();
                 contact.setFirstName(firstName);
 
                 System.out.println("Enter the Last Name : " );
-                String lastName = sc.next();
+               String lastName = sc.next();
                 contact.setLastName(lastName);
 
                 System.out.println("Enter the Address : " );
@@ -41,15 +43,15 @@ public class AddressSystem {
 
                 System.out.println("Enter the State : " );
                 String state = sc.next();
-                contact.setState(state);
+               contact.setState(state);
 
-                System.out.println("Enter the ZIP Code : " );
+               System.out.println("Enter the ZIP Code : " );
                 long zipCode = sc.nextLong();
                 contact.setZip(zipCode);
 
-                System.out.println("Enter the Phone Number : " );
+               System.out.println("Enter the Phone Number : " );
                 long phoneNumber = sc.nextLong();
-               contact.setPhoneNumber(phoneNumber);
+                contact.setPhoneNumber(phoneNumber);
 
                 System.out.println("Enter the E-mail: " );
                 String email = sc.next();
@@ -59,7 +61,7 @@ public class AddressSystem {
 
                 for (Contact c : contactList){
                     System.out.println(c.getFirstName() + " " + c.getLastName() + " " + c.getAddress() + " " + c.getCity() + " " + c.getState() + " " + c.getPhoneNumber() + " " + c.getZip() + " " + c.getEmail());
-               }
+                }
                 break;
 
             case 2 :
@@ -69,15 +71,15 @@ public class AddressSystem {
                 for(int i = 0; i < contactList.size(); i++ ){
                     if (contactList.get(i).getFirstName().equals(name)){
                         System.out.println("Enter the Last Name : " );
-                        String newLastName = sc.next();
-                        contact.setLastName(newLastName);
+                        String newLastName = sc.next();                        contact.setLastName(newLastName);
 
                         System.out.println("Enter the Address : " );
                         String newAddress = sc.next();
                         contact.setAddress(newAddress);
 
                         System.out.println("Enter the City : " );
-                        String newCity = sc.next();                        contact.setCity(newCity);
+                        String newCity = sc.next();
+                        contact.setCity(newCity);
 
                         System.out.println("Enter the State : " );
                         String newState = sc.next();
@@ -89,17 +91,27 @@ public class AddressSystem {
 
                         System.out.println("Enter the Phone Number : " );
                         long newPhoneNumber = sc.nextLong();
-                        contact.setPhoneNumber(newPhoneNumber);
+                       contact.setPhoneNumber(newPhoneNumber);
 
-                        System.out.println("Enter the E-mail: " );
+                       System.out.println("Enter the E-mail: " );
                         String newEmail = sc.next();
                         contact.setEmail(newEmail);
-                   }
+                    }
                 }
-               break;
+                break;
+            case 3 :
+               System.out.println("Delete");
+                System.out.println("Enter the first name : ");
+                String deleteName = sc.next();
+                for(int i = 0; i < contactList.size(); i++){
+                    if(contactList.get(i).getFirstName().equals(deleteName)){
+                       contactList.remove(i);
+                    }
 
-           default:
+                }
+
+            default:
                 System.out.println("Invalid Option");
        }
-   }
+    }
 }
